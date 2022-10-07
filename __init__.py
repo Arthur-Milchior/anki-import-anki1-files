bfb9f2e6509897ec9aaf6fa66999a22375a70ef8
Arthur Milchior <arthur@milchior.fr>
2022-10-07 13:26:21 +0200

    NF: uses hook importing_importer

--- __init__.py
+++ __init__.py
@@ -2,5 +2,11 @@ import sys
 import pathlib
 path = f"{pathlib.Path(__file__).parent.absolute()}/"
 sys.path.append(path)
+from anki import hooks
+from .anki.importing.anki1 import Anki1Importer
+from anki.importing.base import Importer
 
-from .aqt import importing
+def add_anki1_importer(importers: list[tuple[str, type[Importer]]]):
+    importers.append(("Anki 1.2 Deck (*.anki)", Anki1Importer))
+
+hooks.importing_importers.append(add_anki1_importer)
